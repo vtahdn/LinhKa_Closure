16e4238dae5061963769318265f70f537e400aea
Viet Asc <vtahdn@gmail.com>
2018-10-30 17:24:24 +0700

    Initial Commit

---
+++ LinhKa_Closure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinhKa_Closure
+//
+//  Created by Viet Asc on 10/30/18.
+//  Copyright © 2018 Viet Asc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LinhKa_ClosureTests/LinhKa_ClosureTests.swift
@@ -0,0 +1,34 @@
+//
+//  LinhKa_ClosureTests.swift
+//  LinhKa_ClosureTests
+//
+//  Created by Viet Asc on 10/30/18.
+//  Copyright © 2018 Viet Asc. All rights reserved.
+//
+
+import XCTest
+@testable import LinhKa_Closure
+
+class LinhKa_ClosureTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
